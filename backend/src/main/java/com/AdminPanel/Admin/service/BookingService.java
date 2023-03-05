ab54483d4f2cabf39b1989b732ea4df7da663212
Chamithu Ruberu <94827905+tcdruberu@users.noreply.github.com>
2023-03-05 13:56:35 +0530

    e

--- backend/src/main/java/com/AdminPanel/Admin/service/BookingService.java
+++
@@ -1,7 +0,0 @@
-package com.AdminPanel.Admin.service;
-
-import com.AdminPanel.Admin.dto.request.RequestBookingSaveDTO;
-
-public interface BookingService {
-    String addBooking(RequestBookingSaveDTO requestBookingSaveDTO);
-}
